b8aabef31418026aecf0eca62229e17b34139477
Paresh <09patilparesh@gmail.com>
2023-08-28 13:02:29 +0530

    front

---
+++ src/Components/Gallary/Gallary.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./Gallary.css"
+
+function Gallary() {
+    return (
+        <>
+
+            <div className='main_gallary_div'>
+
+                <div className='main_gal_card_div'>
+                    <div className='sub_card_gal newimg1'>
+                        Automobile
+                    </div>
+
+                    <div className='sub_card_gal newimg2'>
+                        Hardware
+                    </div>
+
+                    <div className='sub_card_gal newimg3'>
+                        Infrastructure
+                    </div>
+
+                    <div className='sub_card_gal newimg4'>
+                        Chemical
+                    </div>
+
+                    <div className='sub_card_gal newimg5'>
+                        Real Estate
+                    </div>
+
+                    <div className='sub_card_gal newimg6'>
+                        Milk & Dairy
+                    </div>
+
+                    <div className='sub_card_gal newimg7'>
+                        Crypto
+                    </div>
+
+                    <div className='sub_card_gal newimg8'>
+                        Hospital
+                    </div>
+
+                    <div className='sub_card_gal newimg9'>
+                        Transportation
+                    </div>
+
+                    <div className='sub_card_gal newimg10'>
+                        Electrical
+                    </div>
+
+                    <div className='sub_card_gal newimg11'>
+                        Education
+                    </div>
+
+                    <div className='sub_card_gal newimg12'>
+                        Texttile
+                    </div>
+
+                    <div className='sub_card_gal newimg13'>
+                        Film & Entertainmaint
+                    </div>
+
+                    <div className='sub_card_gal newimg14'>
+                        Telecom
+                    </div>
+
+                    <div className='sub_card_gal newimg15'>
+                        Oil
+                    </div>
+
+                    <div className='sub_card_gal newimg16'>
+
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Gallary
